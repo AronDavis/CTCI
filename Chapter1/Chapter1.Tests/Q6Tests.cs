04eed72aedc6925ca511993c48e84d1c2b25c6ac
Aron <arontd@gmail.com>
2019-03-26 22:23:57 -0400

    1.6

---
+++ Chapter1/Chapter1.Tests/Q6Tests.cs
@@ -0,0 +1,105 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Chapter1.Tests
+{
+    [TestClass]
+    public class Q6Tests
+    {
+        [TestMethod]
+        public void TestNull()
+        {
+            int[][] arr = null;
+            Q6.RotateMatrix90(arr, 0);
+
+            Assert.IsNull(arr);
+
+            arr = new int[1][];
+            Q6.RotateMatrix90(arr, 1);
+
+            Assert.AreEqual(1, arr.Length);
+            Assert.IsNull(arr[0]);
+        }
+
+        [TestMethod]
+        public void TestBadLength()
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 }
+            };
+
+            Q6.RotateMatrix90(arr, 1);
+
+            Assert.AreEqual(2, arr.Length);
+            Assert.IsTrue(arr.All(a => a.Length == 2));
+
+            Assert.AreEqual(1, arr[0][0]);
+            Assert.AreEqual(2, arr[0][1]);
+            Assert.AreEqual(3, arr[1][0]);
+            Assert.AreEqual(4, arr[1][1]);
+        }
+
+        [TestMethod]
+        public void TestSingle()
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] { 1 }
+            };
+
+            Q6.RotateMatrix90(arr, 1);
+
+            Assert.AreEqual(1, arr.Length);
+            Assert.AreEqual(1, arr[0].Length);
+            Assert.AreEqual(1, arr[0][0]);
+        }
+
+        [TestMethod]
+        public void TestEven()
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] { 1, 2 },
+                new int[] { 3, 4 }
+            };
+
+            Q6.RotateMatrix90(arr, 2);
+
+            Assert.AreEqual(2, arr.Length);
+            Assert.IsTrue(arr.All(a => a.Length == 2));
+
+            Assert.AreEqual(1, arr[0][1]);
+            Assert.AreEqual(2, arr[1][1]);
+            Assert.AreEqual(3, arr[0][0]);
+            Assert.AreEqual(4, arr[1][0]);
+        }
+
+        [TestMethod]
+        public void TestOdd()
+        {
+            int[][] arr = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 4, 5, 6 },
+                new int[] { 7, 8, 9 }
+            };
+
+            Q6.RotateMatrix90(arr, 3);
+
+            Assert.AreEqual(3, arr.Length);
+            Assert.IsTrue(arr.All(a => a.Length == 3));
+
+            Assert.AreEqual(1, arr[0][2]);
+            Assert.AreEqual(2, arr[1][2]);
+            Assert.AreEqual(3, arr[2][2]);
+            Assert.AreEqual(4, arr[0][1]);
+            Assert.AreEqual(5, arr[1][1]);
+            Assert.AreEqual(6, arr[2][1]);
+            Assert.AreEqual(7, arr[0][0]);
+            Assert.AreEqual(8, arr[1][0]);
+            Assert.AreEqual(9, arr[2][0]);
+        }
+    }
+}
---
+++ Chapter1/Chapter1/Q6.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace Chapter1
+{
+    public static class Q6
+    {
+        /// <summary>
+        /// Assumes the matrix is square
+        /// </summary>
+        /// <param name="arr"></param>
+        /// <param name="n">Length of the sides.</param>
+        public static void RotateMatrix90(int[][] arr, int n)
+        {
+            if (arr == null || arr.Any(a => a == null) || arr.Length != n || arr.Any(a => a.Length != n))
+                return;
+
+            for(int layer = 0; layer < n/2; layer++)
+            {
+                int first = layer;
+                int last = (n - 1) - layer;
+                for(int i = first; i < last; i++)
+                {
+                    int offset = i - first;
+                    int top = arr[first][i];
+
+                    // left to top
+                    arr[first][i] = arr[last - offset][first];
+
+                    //bottom to left
+                    arr[last - offset][first] = arr[last][last - offset];
+
+                    //right to bottom
+                    arr[last][last - offset] = arr[i][last];
+
+                    //top to right
+                    arr[i][last] = top;
+                }
+            }
+        }
+    }
+}
